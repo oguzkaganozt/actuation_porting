538a420edca187204bd55f35f8a8463da5d1eb83
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-02-12 15:41:36 +0000

    upgrade cyclonedds tand zephyr 3.5 to 3.6 t make it compatible with ROS humble
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+pip3 install -r zephyr/scripts/requirements-base.txt
+pip3 install -r zephyr/scripts/requirements-build-test.txt
+
 west init -l zephyr_app > /dev/null 2>&1
 west update
 west zephyr-export
