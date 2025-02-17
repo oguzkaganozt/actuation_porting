8a42d86be6da53542b22f6c1bd8e9cef77441fef
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-02-17 17:50:12 +0000

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 ROS2_MSG_PATH="/opt/ros/humble/share"
 AWF_MSG_PATH="/opt/autoware/share"
-MESSAGE_MODULES="autoware_planning_msgs autoware_control_msgs autoware_vehicle_msgs autoware_perception_msgs tier4_debug_msgs builtin_interfaces visualization_msgs tier4_planning_msgs autoware_adapi_v1_msgs geometry_msgs std_msgs"
 DEST_PATH="$PWD/actuation_packages/actuation_msgs/msg"
+source actuation_packages/actuation_msgs/messages.env
 
 determine_msg_paths() {
     local msg_paths=""
@@ -29,9 +29,32 @@ copy_messages() {
     echo -e "$msg_paths"
 }
 
+add_include_guard_to_messages() {
+    for idl_file in $(find "$DEST_PATH" -name "*.idl"); do
+        filename=$(basename "$idl_file" .idl)
+        filename_upper=$(echo "$filename" | tr '[:lower:]' '[:upper:]')
+        
+        # Create temporary file
+        temp_file=$(mktemp)
+        
+        # Add header guard and content
+        echo "#ifndef ${filename_upper}_IDL_" > "$temp_file"
+        echo "#define ${filename_upper}_IDL_" >> "$temp_file"
+        echo "" >> "$temp_file"
+        cat "$idl_file" >> "$temp_file"
+        echo "" >> "$temp_file"
+        echo "#endif  // ${filename_upper}_IDL_" >> "$temp_file"
+        
+        # Replace original file with modified content
+        mv "$temp_file" "$idl_file"
+    done
+}
+
+rm -rf $DEST_PATH
 mkdir -p $DEST_PATH
 msg_paths=$(copy_messages)
 echo -e "Copied messages to $DEST_PATH: $msg_paths"
+add_include_guard_to_files
 
 pip3 install -r zephyr/scripts/requirements-base.txt
 pip3 install -r zephyr/scripts/requirements-build-test.txt
