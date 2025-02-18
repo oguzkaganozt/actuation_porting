0da1b3a36b69d108e009e872e87dbbb027c1876b
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-02-18 11:22:13 +0000

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,36 +1,13 @@
 #! /bin/bash
-ROS2_MSG_PATH="/opt/ros/humble/share"
-AWF_MSG_PATH="/opt/autoware/share"
 DEST_PATH="$PWD/actuation_packages/actuation_msgs/msg"
-source actuation_packages/actuation_msgs/messages.env
-
-determine_msg_paths() {
-    local msg_paths=""
-    local path=""
-    
-    for msg_name in $@; do
-        if [[ "$msg_name" == *"autoware"* || "$msg_name" == *"tier4"* ]]; then
-            path="$AWF_MSG_PATH/$msg_name"
-            msg_paths="$msg_paths$path "
-        else
-            path="$ROS2_MSG_PATH/$msg_name"
-            msg_paths="$msg_paths$path "
-        fi
-    done
-    
-    echo -e "$msg_paths"
-}
-
-copy_messages() {
-    local msg_paths=$(determine_msg_paths "$MESSAGE_MODULES")
-    for msg_path in $msg_paths; do
-        find "$msg_path/msg" -name "*.idl" -exec cp {} $DEST_PATH \;
-    done
-    echo -e "$msg_paths"
-}
 
 add_include_guard_to_messages() {
     for idl_file in $(find "$DEST_PATH" -name "*.idl"); do
+        # Check if file already has include guard
+        if grep -q "^#ifndef.*_IDL_" "$idl_file"; then
+            continue  # Skip this file if include guard exists
+        fi
+
         filename=$(basename "$idl_file" .idl)
         filename_upper=$(echo "$filename" | tr '[:lower:]' '[:upper:]')
         
@@ -50,11 +27,15 @@ add_include_guard_to_messages() {
     done
 }
 
-rm -rf $DEST_PATH
-mkdir -p $DEST_PATH
-msg_paths=$(copy_messages)
-echo -e "Copied messages to $DEST_PATH: $msg_paths"
+fix_include_paths() {
+    for idl_file in $(find "$DEST_PATH" -name "*.idl"); do
+        # Use sed to directly modify include lines in place
+        sed -i 's|#include "[^"]*\/\([^/"]*\.idl\)"|#include "\1"|g' "$idl_file"
+    done
+}
+
 add_include_guard_to_messages
+fix_include_paths
 
 pip3 install -r zephyr/scripts/requirements-base.txt
 pip3 install -r zephyr/scripts/requirements-build-test.txt
