74d5324198d08fea041e5ba9028b7f88360b8b6d
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-02-24 11:30:32 +0000

    add message definitions
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- zephyr_app/include/messages.h
+++ zephyr_app/include/messages.h
@@ -11,7 +11,9 @@
 #include "Transform.h"
 #include "TransformStamped.h"
 #include "Twist.h"
+#include "TwistStamped.h"
 #include "TwistWithCovariance.h"
+#include "Odometry.h"
 #include "Vector3.h"
 #include "MarkerArray.h"
 #include "PathPoint.h"
@@ -26,11 +28,32 @@ using geometryMsgsPoseStamped = geometry_msgs_msg_PoseStamped;
 using geometryMsgsPoseWithCovarianceStamped = geometry_msgs_msg_PoseWithCovarianceStamped;
 using geometryMsgsQuaternion = geometry_msgs_msg_Quaternion;
 using geometryMsgsTransformStamped = geometry_msgs_msg_TransformStamped;
+using geometryMsgsTwist = geometry_msgs_msg_Twist;
+using geometryMsgsTwistStamped = geometry_msgs_msg_TwistStamped;
+using navMsgsOdometry = nav_msgs_msg_Odometry;
 using visualizationMsgsMarkerArray = visualization_msgs_msg_MarkerArray;
 using awfPlanningMsgsTrajectory = autoware_planning_msgs_msg_Trajectory;
 using awfPlanningMsgsTrajectoryPoint = autoware_planning_msgs_msg_TrajectoryPoint;
 using awfPlanningMsgsPathPoint = autoware_planning_msgs_msg_PathPoint;
 using tier4PlanningMsgsPathPointWithLaneId = tier4_planning_msgs_msg_PathPointWithLaneId;
 using geometryMsgsTransform = geometry_msgs_msg_Transform;
+
+// Simplified message names
+using PointMsg = geometryMsgsPoint;
+using PoseMsg = geometryMsgsPose;
+using Vector3Msg = geometryMsgsVector3;
+using PoseStampedMsg = geometryMsgsPoseStamped;
+using PoseWithCovarianceStampedMsg = geometryMsgsPoseWithCovarianceStamped;
+using QuaternionMsg = geometryMsgsQuaternion;
+using TransformStampedMsg = geometryMsgsTransformStamped;
+using TwistMsg = geometryMsgsTwist;
+using TwistStampedMsg = geometryMsgsTwistStamped;
+using OdometryMsg = navMsgsOdometry;
+using MarkerArrayMsg = visualizationMsgsMarkerArray;
+using TrajectoryMsg = awfPlanningMsgsTrajectory;
+using TrajectoryPointMsg = awfPlanningMsgsTrajectoryPoint;
+using PathPointMsg = awfPlanningMsgsPathPoint;
+using PathPointWithLaneIdMsg = tier4PlanningMsgsPathPointWithLaneId;
+using TransformMsg = geometryMsgsTransform;
 
 #endif  // ACTUATION_AUTOWARE__MESSAGES_H_
