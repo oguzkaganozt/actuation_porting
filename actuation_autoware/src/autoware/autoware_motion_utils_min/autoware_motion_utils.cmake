6869944ef89302bb676be52da822c2a65923ed80
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-02-28 16:16:01 +0000

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- actuation_autoware/src/autoware/autoware_motion_utils_min/autoware_motion_utils.cmake
+++ actuation_autoware/src/autoware/autoware_motion_utils_min/autoware_motion_utils.cmake
@@ -2,7 +2,6 @@
 # Add source files
 list(APPEND APP_SOURCES
   src/autoware/autoware_motion_utils_min/src/trajectory/trajectory.cpp
-  src/autoware/autoware_motion_utils_min/src/vehicle/vehicle_state_checker.cpp
 )
 
 # Add include directories
