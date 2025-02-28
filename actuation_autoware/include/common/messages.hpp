63f1a18b822ec9cd3adc3427b5ffe09e7a901fbd
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-02-28 13:19:02 +0000

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- actuation_autoware/include/common/messages.hpp
+++ actuation_autoware/include/common/messages.hpp
@@ -1,6 +1,8 @@
 #ifndef ACTUATION_AUTOWARE__MESSAGES_H_
 #define ACTUATION_AUTOWARE__MESSAGES_H_
 
+#include "common/sequence.hpp"
+
 #include "Point.h"
 #include "Path.h"
 #include "Trajectory.h"
@@ -48,8 +50,6 @@
 #include "OperationModeState.h"
 #include "Engage.h"
 
-#include "common/sequence.hpp"
-
 // Message types from autoware_msgs
 using PointMsg = geometry_msgs_msg_Point;
 using PoseMsg = geometry_msgs_msg_Pose;
@@ -76,56 +76,63 @@ using OperationModeStateMsg = autoware_adapi_v1_msgs_msg_OperationModeState;
 using EngageMsg = autoware_vehicle_msgs_msg_Engage;
 
 // Raw Sequence types from autoware_msgs
-using awfPlanningMsgsSequenceTrajectoryPoint = dds_sequence_autoware_planning_msgs_msg_TrajectoryPoint;
-using awfPerceptionMsgsSequenceObjectClassification = dds_sequence_autoware_perception_msgs_msg_ObjectClassification;
-using awfPerceptionMsgsSequenceDetectedObject = dds_sequence_autoware_perception_msgs_msg_DetectedObject;
-using awfPlanningMsgsSequenceLaneletSegment = dds_sequence_autoware_planning_msgs_msg_LaneletSegment;
-using awfPlanningMsgsSequenceLaneletPrimitive = dds_sequence_autoware_planning_msgs_msg_LaneletPrimitive;
-using visualizationMsgsSequenceUVCoordinate = dds_sequence_visualization_msgs_msg_UVCoordinate;
-using visualizationMsgsSequenceMarker = dds_sequence_visualization_msgs_msg_Marker;
-using tier4DebugMsgsSequenceMultiArrayDimension = dds_sequence_tier4_debug_msgs_msg_MultiArrayDimension;
-using awfPlanningMsgsSequencePathPoint = dds_sequence_autoware_planning_msgs_msg_PathPoint;
-using tier4PlanningMsgsSequencePathPointWithLaneId = dds_sequence_tier4_planning_msgs_msg_PathPointWithLaneId;
-using geometryMsgsSequencePoint32 = dds_sequence_geometry_msgs_msg_Point32;
-using geometryMsgsSequencePose = dds_sequence_geometry_msgs_msg_Pose;
-using awfPerceptionMsgsSequencePredictedPath = dds_sequence_autoware_perception_msgs_msg_PredictedPath;
-using awfPerceptionMsgsSequencePredictedObject = dds_sequence_autoware_perception_msgs_msg_PredictedObject;
-using tier4DebugMsgsSequenceProcessingTimeNode = dds_sequence_tier4_debug_msgs_msg_ProcessingTimeNode;
-using tier4DebugMsgsSequenceSystemUsage = dds_sequence_tier4_debug_msgs_msg_SystemUsage;
-using awfPerceptionMsgsSequenceTrackedObject = dds_sequence_autoware_perception_msgs_msg_TrackedObject;
-using awfPerceptionMsgsSequenceTrafficLightElement = dds_sequence_autoware_perception_msgs_msg_TrafficLightElement;
-using awfPerceptionMsgsSequenceTrafficLightGroup = dds_sequence_autoware_perception_msgs_msg_TrafficLightGroup;
-using awfPerceptionMsgsSequenceTrafficSignalElement = dds_sequence_autoware_perception_msgs_msg_TrafficSignalElement;
-using awfPerceptionMsgsSequenceTrafficSignal = dds_sequence_autoware_perception_msgs_msg_TrafficSignal;
+using FloatSeqRaw = dds_sequence_float;
+using DoubleSeqRaw = dds_sequence_double;
+using UInt8SeqRaw = dds_sequence_uint8;
+using Int32SeqRaw = dds_sequence_int32;
+using Int64SeqRaw = dds_sequence_int64;
+using PointSeqRaw = dds_sequence_geometry_msgs_msg_Point;
+using ColorRGBASeqRaw = dds_sequence_std_msgs_msg_ColorRGBA;
+using TrajectoryPointSeqRaw = dds_sequence_autoware_planning_msgs_msg_TrajectoryPoint;
+using ObjectClassificationSeqRaw = dds_sequence_autoware_perception_msgs_msg_ObjectClassification;
+using DetectedObjectSeqRaw = dds_sequence_autoware_perception_msgs_msg_DetectedObject;
+using LaneletSegmentSeqRaw = dds_sequence_autoware_planning_msgs_msg_LaneletSegment;
+using LaneletPrimitiveSeqRaw = dds_sequence_autoware_planning_msgs_msg_LaneletPrimitive;
+using UVCoordinateSeqRaw = dds_sequence_visualization_msgs_msg_UVCoordinate;
+using MarkerSeqRaw = dds_sequence_visualization_msgs_msg_Marker;
+using MultiArrayDimensionSeqRaw = dds_sequence_tier4_debug_msgs_msg_MultiArrayDimension;
+using PathPointSeqRaw = dds_sequence_autoware_planning_msgs_msg_PathPoint;
+using PathPointWithLaneIdSeqRaw = dds_sequence_tier4_planning_msgs_msg_PathPointWithLaneId;
+using Point32SeqRaw = dds_sequence_geometry_msgs_msg_Point32;
+using PoseSeqRaw = dds_sequence_geometry_msgs_msg_Pose;
+using PredictedPathSeqRaw = dds_sequence_autoware_perception_msgs_msg_PredictedPath;
+using PredictedObjectSeqRaw = dds_sequence_autoware_perception_msgs_msg_PredictedObject;
+using ProcessingTimeNodeSeqRaw = dds_sequence_tier4_debug_msgs_msg_ProcessingTimeNode;
+using SystemUsageSeqRaw = dds_sequence_tier4_debug_msgs_msg_SystemUsage;
+using TrackedObjectSeqRaw = dds_sequence_autoware_perception_msgs_msg_TrackedObject;
+using TrafficLightElementSeqRaw = dds_sequence_autoware_perception_msgs_msg_TrafficLightElement;
+using TrafficLightGroupSeqRaw = dds_sequence_autoware_perception_msgs_msg_TrafficLightGroup;
+using TrafficSignalElementSeqRaw = dds_sequence_autoware_perception_msgs_msg_TrafficSignalElement;
+using TrafficSignalSeqRaw = dds_sequence_autoware_perception_msgs_msg_TrafficSignal;
 
 // Wrapped sequence types
-using FloatSeq = Sequence<dds_sequence_float>;
-using DoubleSeq = Sequence<dds_sequence_double>;
-using UInt8Seq = Sequence<dds_sequence_uint8>;
-using Int32Seq = Sequence<dds_sequence_int32>;
-using Int64Seq = Sequence<dds_sequence_int64>;
-using PointSeq = Sequence<dds_sequence_geometry_msgs_msg_Point>;
-using ColorRGBASeq = Sequence<dds_sequence_std_msgs_msg_ColorRGBA>;
-using ObjectClassificationSeq = Sequence<awfPerceptionMsgsSequenceObjectClassification>;
-using DetectedObjectSeq = Sequence<awfPerceptionMsgsSequenceDetectedObject>;
-using TrajectoryPointSeq = Sequence<awfPlanningMsgsSequenceTrajectoryPoint>;
-using LaneletSegmentSeq = Sequence<awfPlanningMsgsSequenceLaneletSegment>;
-using LaneletPrimitiveSeq = Sequence<awfPlanningMsgsSequenceLaneletPrimitive>;
-using UVCoordinateSeq = Sequence<visualizationMsgsSequenceUVCoordinate>;
-using MarkerSeq = Sequence<visualizationMsgsSequenceMarker>;
-using MultiArrayDimensionSeq = Sequence<tier4DebugMsgsSequenceMultiArrayDimension>;
-using PathPointSeq = Sequence<awfPlanningMsgsSequencePathPoint>;
-using PathPointWithLaneIdSeq = Sequence<tier4PlanningMsgsSequencePathPointWithLaneId>;
-using Point32Seq = Sequence<geometryMsgsSequencePoint32>;
-using PoseSeq = Sequence<geometryMsgsSequencePose>;
-using PredictedPathSeq = Sequence<awfPerceptionMsgsSequencePredictedPath>;
-using PredictedObjectSeq = Sequence<awfPerceptionMsgsSequencePredictedObject>;
-using ProcessingTimeNodeSeq = Sequence<tier4DebugMsgsSequenceProcessingTimeNode>;
-using SystemUsageSeq = Sequence<tier4DebugMsgsSequenceSystemUsage>;
-using TrackedObjectSeq = Sequence<awfPerceptionMsgsSequenceTrackedObject>;
-using TrafficLightElementSeq = Sequence<awfPerceptionMsgsSequenceTrafficLightElement>;
-using TrafficLightGroupSeq = Sequence<awfPerceptionMsgsSequenceTrafficLightGroup>;
-using TrafficSignalElementSeq = Sequence<awfPerceptionMsgsSequenceTrafficSignalElement>;
-using TrafficSignalSeq = Sequence<awfPerceptionMsgsSequenceTrafficSignal>;
+using FloatSeq = Sequence<FloatSeqRaw>;
+using DoubleSeq = Sequence<DoubleSeqRaw>;
+using UInt8Seq = Sequence<UInt8SeqRaw>;
+using Int32Seq = Sequence<Int32SeqRaw>;
+using Int64Seq = Sequence<Int64SeqRaw>;
+using PointSeq = Sequence<PointSeqRaw>;
+using ColorRGBASeq = Sequence<ColorRGBASeqRaw>;
+using ObjectClassificationSeq = Sequence<ObjectClassificationSeqRaw>;
+using DetectedObjectSeq = Sequence<DetectedObjectSeqRaw>;
+using TrajectoryPointSeq = Sequence<TrajectoryPointSeqRaw>;
+using LaneletSegmentSeq = Sequence<LaneletSegmentSeqRaw>;
+using LaneletPrimitiveSeq = Sequence<LaneletPrimitiveSeqRaw>;
+using UVCoordinateSeq = Sequence<UVCoordinateSeqRaw>;
+using MarkerSeq = Sequence<MarkerSeqRaw>;
+using MultiArrayDimensionSeq = Sequence<MultiArrayDimensionSeqRaw>;
+using PathPointSeq = Sequence<PathPointSeqRaw>;
+using PathPointWithLaneIdSeq = Sequence<PathPointWithLaneIdSeqRaw>;
+using Point32Seq = Sequence<Point32SeqRaw>;
+using PoseSeq = Sequence<PoseSeqRaw>;
+using PredictedPathSeq = Sequence<PredictedPathSeqRaw>;
+using PredictedObjectSeq = Sequence<PredictedObjectSeqRaw>;
+using ProcessingTimeNodeSeq = Sequence<ProcessingTimeNodeSeqRaw>;
+using SystemUsageSeq = Sequence<SystemUsageSeqRaw>;
+using TrackedObjectSeq = Sequence<TrackedObjectSeqRaw>;
+using TrafficLightElementSeq = Sequence<TrafficLightElementSeqRaw>;
+using TrafficLightGroupSeq = Sequence<TrafficLightGroupSeqRaw>;
+using TrafficSignalElementSeq = Sequence<TrafficSignalElementSeqRaw>;
+using TrafficSignalSeq = Sequence<TrafficSignalSeqRaw>;
 
 #endif  // ACTUATION_AUTOWARE__MESSAGES_H_
