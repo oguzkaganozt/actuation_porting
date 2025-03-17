ce8973f0018369474301d7d3163d7dc1f0e5b50d
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-03-17 18:31:35 +0000

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

---
+++ actuation_autoware/src/autoware/autoware_trajectory_follower_node/autoware_trajectory_follower_node.cmake
@@ -0,0 +1,10 @@
+
+# Add source files
+list(APPEND APP_SOURCES
+  src/autoware/autoware_trajectory_follower_base/src/lateral_controller_base.cpp
+)
+
+# Add include directories
+list(APPEND APP_INCLUDE_DIRS
+  src/autoware/autoware_trajectory_follower_base/include
+)
