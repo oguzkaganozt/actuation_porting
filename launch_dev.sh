5906ad5895970b9b7d1bdfa8a7deae7746233d2a
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-05-05 14:54:38 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- launch_dev.sh
+++ launch_dev.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
 # Run docker with user mapping
-docker run --rm -it \
+docker run --rm -it --name zephyr-dev \
+    --network host \
     -v "$HOME/.ccache:/root/.ccache" \
     -v "$(pwd):/actuation" \
-    --name zephyr-dev \
     -w "/actuation" \
     -e CCACHE_DIR=/root/.ccache \
     -e CYCLONEDDS_URI="file:///actuation/cyclonedds.xml" \
     -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
     -e ROS_DOMAIN_ID=2 \
-    --network host \
     zephyr-dev
