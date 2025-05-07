6abcf15fe734545bd05b4e7f310aa8371f9ab3d9
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-05-07 17:57:12 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -5,20 +5,27 @@ COLOR_BLUE="\e[34m"
 COLOR_GREEN="\e[32m"
 COLOR_RESET="\e[0m"
 
+# Source ROS and Autoware
 source "/opt/ros/humble/setup.bash"
 source "/opt/autoware/setup.bash"
 
+# Clone submodules
+git submodule update --init --recursive
+
+# Install Zephyr dependencies
 echo -e "${COLOR_BLUE}Checking Zephyr dependencies...${COLOR_RESET}"
 pip3 install -r zephyr/scripts/requirements-base.txt > /dev/null 2>&1
 pip3 install -r zephyr/scripts/requirements-build-test.txt > /dev/null 2>&1
 
+# Initialize and update Zephyr project
 echo -e "${COLOR_BLUE}Initializing and updating Zephyr project...${COLOR_RESET}"
 west init -l actuation_module > /dev/null 2>&1
 west update > /dev/null 2>&1
 
+# Export Zephyr SDK
 echo -e "${COLOR_BLUE}Exporting Zephyr SDK...${COLOR_RESET}"
 west zephyr-export > /dev/null 2>&1
 
+# Ready to go!
 echo -e "${COLOR_GREEN}Dev container ready!${COLOR_RESET}"
-
 exec "/bin/bash"
