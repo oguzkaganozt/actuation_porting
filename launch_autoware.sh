89e357d40164429e1b849fd349992c91dee0fcfd
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-05-07 17:01:14 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- launch_autoware.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xhost +
-
-# Run docker with user mapping
-docker run -it --rm -e DISPLAY="$DISPLAY" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v "$HOME"/autoware_map/sample-map-planning:/root/autoware_map/sample-map-planning \
-    ghcr.io/autowarefoundation/autoware:universe-20250207 /bin/bash
