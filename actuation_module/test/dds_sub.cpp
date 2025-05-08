df61ac77193074124ae9aff123a837ef6c03510b
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-05-08 11:29:58 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- actuation_module/test/dds_sub.cpp
+++ actuation_module/test/dds_sub.cpp
@@ -4,15 +4,6 @@
 #include "common/logger/logger.hpp"
 using namespace common::logger;
 
-#if defined(NATIVE_SIM)
-static unsigned char node_stack[CONFIG_THREAD_STACK_SIZE];
-static unsigned char timer_stack[CONFIG_THREAD_STACK_SIZE];
-#else
-static K_THREAD_STACK_DEFINE(node_stack, CONFIG_THREAD_STACK_SIZE);
-static K_THREAD_STACK_DEFINE(timer_stack, CONFIG_THREAD_STACK_SIZE);
-#define STACK_SIZE (K_THREAD_STACK_SIZEOF(node_stack))
-#endif
-
 // Msgs
 #include "SteeringReport.h"
 #include "Trajectory.h"
@@ -24,6 +15,16 @@ using TrajectoryMsg = autoware_planning_msgs_msg_Trajectory;
 using OdometryMsg = nav_msgs_msg_Odometry;
 using AccelerationMsg = geometry_msgs_msg_AccelWithCovarianceStamped;
 using OperationModeStateMsg = autoware_adapi_v1_msgs_msg_OperationModeState;
+
+// Stack sizes for node and timer threads
+#if defined(NATIVE_SIM)
+static unsigned char node_stack[CONFIG_THREAD_STACK_SIZE];
+static unsigned char timer_stack[CONFIG_THREAD_STACK_SIZE];
+#else
+static K_THREAD_STACK_DEFINE(node_stack, CONFIG_THREAD_STACK_SIZE);
+static K_THREAD_STACK_DEFINE(timer_stack, CONFIG_THREAD_STACK_SIZE);
+#define STACK_SIZE (K_THREAD_STACK_SIZEOF(node_stack))
+#endif
 
 /*
     This test is used to test the DDS communication between ROS2 and Zephyr
