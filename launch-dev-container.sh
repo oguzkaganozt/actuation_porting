7b652ffa864728bec3b8f57456a66c5b7a47c61d
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-09-19 16:07:32 +0300

    add domain bridge
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- launch-dev-container.sh
+++ launch-dev-container.sh
@@ -20,7 +20,7 @@ docker run --rm -it --name actuation-devcontainer \
     -e CCACHE_DIR=/root/.ccache \
     -e CYCLONEDDS_URI="file:///actuation/cyclonedds.xml" \
     -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
-    -e ROS_DOMAIN_ID=2 \
+    -e ROS_DOMAIN_ID=1 \
     -e DISPLAY="$DISPLAY" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     ghcr.io/oguzkaganozt/devcontainer:latest
