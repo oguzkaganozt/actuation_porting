07773b11a46eab047a9ac893e02e928a692e231c
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-10-01 12:00:19 +0300

    tidy up test directory
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- actuation_module/test/rosbag_test/play_rosbag.sh
+++ actuation_module/test/rosbag_test/play_rosbag.sh
@@ -4,7 +4,7 @@
 source "/opt/ros/humble/setup.bash"
 source "/opt/autoware/setup.bash"
 
-ros2 bag play --start-paused /actuation/actuation_module/test/rosbags/150ms \
+ros2 bag play --start-paused /actuation/actuation_module/test/rosbag_test/rosbag \
     --topics \
     /vehicle/status/steering_status \
     /planning/scenario_planning/trajectory \
